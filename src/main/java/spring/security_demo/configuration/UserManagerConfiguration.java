7803967528f65c32853e5bfa91f3600684b5e67e
butter20010 <butterfly.2006@yandex.ru>
2025-03-19 11:34:50 +0400

    add h2 database, add JdbcUserDetailsManager, add preload data

--- src/main/java/spring/security_demo/configuration/UserManagerConfiguration.java
+++ src/main/java/spring/security_demo/configuration/UserManagerConfiguration.java
@@ -7,20 +7,20 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import spring.security_demo.configuration.user_details_service.InMemoryUserDetailsService;
 import spring.security_demo.configuration.user_details_service.User;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @Configuration
 public class UserManagerConfiguration {
 
   @Bean
-  public UserDetailsService userDetailsService() {
+  public UserDetailsService userDetailsService(DataSource dataSource) {
 
-    UserDetails user = new User("user", "user", "read");
-    List<UserDetails> users = List.of(user);
-    return new InMemoryUserDetailsService(users);
+    return new JdbcUserDetailsManager(dataSource);
   }
 
   @Bean
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `authorities`
+(username, authority)
+VALUES
+('user', 'write');
+INSERT INTO `users`
+(username, password, enabled)
+VALUES
+('user', 'user', '1');
